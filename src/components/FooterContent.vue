8e1334efda3a6db44361a5793498879b7cea4287
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-14 14:10:42 +0100

    Initial commit: importing contents and components

---
+++ src/components/FooterContent.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "FooterContent"
+}
+</script>
+
+<template>
+    <p class="p-5 text-center copyright">
+        Copyright Breaking Bad &copy;
+    </p>
+</template>
+
+<style lang="scss" scoped>
+    .copyright {
+        color: white;
+    }
+    
+</style>
---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: "HeaderContent"
+}
+</script>
+
+<template>
+    <div class="header_container p-3">
+        <img class="header_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Breaking_Bad_logo.svg/1280px-Breaking_Bad_logo.svg.png" alt="Breaking Bad Logo.">
+        <span class="header_title px-3">
+            Breaking Bad Api
+        </span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.header_logo {
+    width: 150px;
+}
+.header_title {
+    font-size: 35px;
+    font-weight: 600;
+    color: white;
+}
+
+</style>
